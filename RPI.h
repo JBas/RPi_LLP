61f2f89ec5c1e366326e5ba88516ef2086fb3ba3
Joshua Bas <joshua.n.bas@gmail.com>
2018-02-01 14:24:27 -0500

    added GPIO macros

--- RPI.h
+++ RPI.h
@@ -15,6 +15,15 @@
 // @TODO: figure out what this is for
 #define BLOCK_SIZE          (4*1024)
 
+// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x)
+#define INP_GPIO(g)         *(gpio.addr + (g/10)) &= ~(7 << ((g%10) * 3))
+#define OUT_GPIO(g)         *(gpio.addr + (g/10)) |= (1 << ((g%10) * 3))
+#define SET_GPIO_ALT(g, a)  *(gpio.addr + (g/10)) |= (((a) <= 3 ? (a) + 4 : (a) == 4 ? 3 : 2) << (((g)%10) * 3))
+#define GPIO_SET            *(gpio.addr + 7) // sets bits that are 1, ignores those that are 0
+#define GPIO_CLR            *(gpio.addr + 10) // sets clears bits which are 1, ignore those that are 0
+
+#define GPIO_READ(g)        *(gpio.addr + 13) &= (1 << g)
+
 // IO Access
 struct bcm2835_peripheral_header {
     unsigned ling addr_p;
